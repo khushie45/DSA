98fa2d7e0a98cfdd095a31c024e427ffb9a4905d
Khushi Patel <77206916+khushie45@users.noreply.github.com>
2022-09-11 21:52:06 +0530

    DAY - 2 Binary Search

---
+++ DSA Python/binary_search.py
@@ -0,0 +1,21 @@
+def binarysearch(arr,x):
+    l,r = 0,len(arr)
+    
+    while l<=r:
+        mid = (l+r)//2
+        if x > arr[mid]:
+            l = mid + 1
+        elif x < arr[mid]:
+            r = mid - 1
+        else:
+            return mid
+    return -1        
+            
+#binary search is applicable for sorted array only.
+arr = [12,34,56,67,78,89,92,95]
+x = int(input("Enter x:"))
+
+print("Element",x,"is found at index",binarysearch(arr,x))
+
+#index -1 means element not found.
+#time complexity of binary search is O(log(n))
